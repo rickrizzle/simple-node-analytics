a5950c2524ee223c91128a6539037bc2455b0aa9
Steffen Kühne <stekhn@gmail.com>
2018-06-28 16:05:51 +0200

    Add Babel and Webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: path.resolve('./client/analytics.js')
+  },
+
+  output: {
+    filename: 'client.bundle.js',
+    path: path.resolve('./')
+  },
+
+  devtool: '#cheap-source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  }
+};
