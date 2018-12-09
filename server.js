bfb79578ab2038f92c76a7d2c91855d4a5b78ce2
Steffen Kühne <stekhn@gmail.com>
2018-12-09 17:58:06 +0100

    Update server code style

--- server.js
+++ server.js
@@ -12,74 +12,64 @@ const databaseName = 'analytics';
 let db;
 
 router.route('/track').post((req, res) => {
-
   if (req.body) {
-
-    const json = ((body) => {
-
+    const json = (body => {
       try {
-
         return JSON.parse(body);
       } catch (err) {
-
         return undefined;
       }
     })(req.body);
 
     if (json) {
-
       insert(json);
       res.sendStatus(201).end();
     } else {
-
       res.sendStatus(400).end();
     }
   }
 });
 
 router.get('/', (req, res) => {
-
   res.json({ message: 'Simple Node Analytics API is up and running.' });
 });
 
 function insert(data) {
-
   if (data.project) {
     // Aggregate values per tracking variable
-    db.collection(data.project)
-      .updateOne(
-        {
-          key: data.key,
-          device: data.device
-        },
-        {
-          $inc: {
-            value: data.value
-          }
-        },
-        {
-          upsert: true
-        },
-        () => {
-          // Callback
+    db.collection(data.project).updateOne(
+      {
+        key: data.key,
+        device: data.device
+      },
+      {
+        $inc: {
+          value: data.value
         }
-      );
+      },
+      {
+        upsert: true
+      },
+      () => {
+        // Callback
+      }
+    );
   }
 }
 
 // Allow Cross-origin request
 app.use((req, res, next) => {
-
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, Content-Length, X-Requested-With'
+  );
 
   // intercept OPTIONS method
   if (req.method === 'OPTIONS') {
-
     res.sendStatus(200);
   } else {
-
     next();
   }
 });
@@ -89,14 +79,18 @@ app.use(bodyParser.text());
 app.use('/build', express.static('build'));
 app.use('/', router);
 
-MongoClient.connect(mongoUrl, { useNewUrlParser: true }, (error, client) => {
+MongoClient.connect(
+  mongoUrl,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) {
+      console.error(error);
+    }
 
-  if (error) { console.error(error); }
+    db = client.db(databaseName);
 
-  db = client.db(databaseName);
-
-  app.listen(port, () => {
-
-    console.log(`Server is running on ${port}`);
-  });
-});
+    app.listen(port, () => {
+      console.log(`Server is running on ${port}`);
+    });
+  }
+);
